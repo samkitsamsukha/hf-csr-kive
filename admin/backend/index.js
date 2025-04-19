c28d09a3a46726173127a0ec33f5f293e38f1050
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-04-20 04:22:58 +0530

    jspdf int

--- admin/backend/index.js
+++ admin/backend/index.js
@@ -22,29 +22,6 @@ app.use(
 app.use("/api/admin", adminRoutes);
 app.use("/api/employee", employeeRoutes);
 
-app.get("/send-report", async (req, res) => {
-	try {
-		// Step 1: Fetch admin data from your local Node.js API
-		const localResponse = await axios.get("http://localhost:4000/api/admin");
-		const adminData = localResponse.data;
-
-		// Step 2: Send that data to the Flask app
-		const flaskUrl = "http://cf6f-117-236-190-193.ngrok-free.app";
-		const flaskResponse = await axios.post(flaskUrl, adminData);
-
-		// Step 3: Return the response from the Flask app to the client
-		return res.status(200).json({
-			message: "Report sent successfully to Python app",
-			response: flaskResponse.data,
-		});
-	} catch (error) {
-		return res.status(500).json({
-			message: "Error during report generation",
-			error: error.message,
-		});
-	}
-});
-
 initializeAdmin();
 
 const port = process.env.PORT || 4000;
